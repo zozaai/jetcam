1f03b7df10916865a815daa78a52f7de12648fbf
SHi.TU23 <shi.tu2323@gmail.com>
2025-09-02 16:28:11 +0200

    fix the import test image in BLIP

--- src/vlms/blip.py
+++ src/vlms/blip.py
@@ -1,15 +1,15 @@
-import requests
-from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 import cv2
+import os
+
+current_dir = os.path.dirname(__file__)
+image_path = os.path.join(os.path.dirname(current_dir), 'pics', 'test.jpg')
 
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
-# img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
-# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-
 raw_image = cv2.imread('./test.jpg').astype('uint8')
+raw_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2RGB)
 
 gen_kwargs = dict(
     do_sample=False,          # no sampling
@@ -23,9 +23,8 @@ gen_kwargs = dict(
 )
 
 # conditional image captioning
-prompt = "explain the image with as much detail as possible"
+prompt = "the image shows "
 inputs = processor(raw_image, return_tensors="pt")
-
 
 out = model.generate(**inputs, **gen_kwargs)
 print("first out: ", processor.decode(out[0], skip_special_tokens=True))
